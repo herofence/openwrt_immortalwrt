93aefa5313d37e61540ce6eec05c78b33feb2655
herofence <113574064+herofence@users.noreply.github.com>
2026-08-28 21:56:25 +0800

    Update default LAN IP address in diy-part2.sh

--- scripts/diy-part2.sh
+++ scripts/diy-part2.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-#
-# Modify default IP
-sed -i 's/192.168.1.1/192.168.11.1/g' package/base-files/files/bin/config_generate
-sed -i "s/ImmortalWrt/OpenWrt/g" package/base-files/files/bin/config_generate
+# Shared post-feed customisation. Runs inside the source tree, after
+# `feeds install` and after .config is in place.
+
+# Default LAN address.
+sed -i 's/192.168.1.1/192.168.31.111/g' package/base-files/files/bin/config_generate
