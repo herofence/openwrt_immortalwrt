1a374f14995bef79141cdf19267abb29bcf58613
herofence <113574064+herofence@users.noreply.github.com>
2026-08-29 12:43:26 +0800

    Update default IP and boot partition size

--- scripts/diy-part2.sh
+++ scripts/diy-part2.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-#修改默认IP
+#1、修改默认IP
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
+# 2. 更改boot分区大小为1M
+sed -i 's/256/1024/g' target/linux/x86/image/Makefile
