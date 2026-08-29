145b8588d3c63c475e684a5142599447773528e2
herofence <113574064+herofence@users.noreply.github.com>
2026-08-29 12:44:27 +0800

    Add comment for software source modification
    Add comment for modifying software sources.

--- scripts/diy-part1.sh
+++ scripts/diy-part1.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+#软件源修改
 # 1. 设置 Git 使用 token 认证（如果存在）
 if [ -n "${GITHUB_TOKEN}" ]; then
     git config --global url."https://oauth2:${GITHUB_TOKEN}@github.com/".insteadOf "https://github.com/"
