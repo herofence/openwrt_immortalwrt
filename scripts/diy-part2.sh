8259a7b2e0b1b26007617c35dfdb071d59220e6e
herofence <113574064+herofence@users.noreply.github.com>
2026-08-29 11:02:47 +0800

    Change default LAN IP address in diy-part2.sh

--- scripts/diy-part2.sh
+++ scripts/diy-part2.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-# Shared post-feed customisation. Runs inside the source tree, after
-# `feeds install` and after .config is in place.
-
-# Default LAN address.
-sed -i 's/192.168.1.1/192.168.31.111/g' package/base-files/files/bin/config_generate
+#修改默认IP
+sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
