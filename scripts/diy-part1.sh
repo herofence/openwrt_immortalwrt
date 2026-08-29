2758f3ae8ca2797bdcd0b6203ca2d5472e7c570e
herofence <113574064+herofence@users.noreply.github.com>
2026-08-29 12:43:07 +0800

    Update diy-part1.sh

--- scripts/diy-part1.sh
+++ scripts/diy-part1.sh
@@ -5,10 +5,7 @@ if [ -n "${GITHUB_TOKEN}" ]; then
     git config --global url."https://oauth2:${GITHUB_TOKEN}@github.com/".insteadOf "https://github.com/"
 fi
 
-# 2. 更改boot分区大小为1M
-sed -i 's/256/1024/g' target/linux/x86/image/Makefile
-
-# 3. 克隆第三方插件包到 package/community 目录
+# 2. 克隆第三方插件包到 package/community 目录
 mkdir -p package/community
 pushd package/community
 # 主题与常用应用
@@ -30,27 +27,27 @@ git clone --depth=1 https://github.com/pymumu/luci-app-smartdns
 git clone --depth=1 https://github.com/vernesong/OpenClash.git
 popd
 
-# 4. 更改默认主题为 Argon
+# 3. 更改默认主题为 Argon
 sed -i 's/luci-theme-design/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
-# 5. x86 型号只显示 CPU 型号
+# 4. x86 型号只显示 CPU 型号
 if [ -f "package/immortalwrt/autocore/files/x86/autocore" ]; then
     sed -i 's/${g}.*/${a}${b}${c}${d}${e}${f}${hydrid}/g' package/immortalwrt/autocore/files/x86/autocore
 elif [ -f "package/lean/autocore/files/x86/autocore" ]; then
     sed -i 's/${g}.*/${a}${b}${c}${d}${e}${f}${hydrid}/g' package/lean/autocore/files/x86/autocore
 fi
 
-# 6. 修改本地时间格式
+# 5. 修改本地时间格式
 find package/ -path "*/autocore/files/*/index.htm" -exec sed -i 's#os.date()#os.date("%Y-%m-%d %H:%M:%S") .. " " .. translate(os.date("%A"))#g' {} \; 2>/dev/null
 find feeds/luci/ -path "*/system.lua" -exec sed -i 's/os.date("%c")/os.date("%Y-%m-%d %H:%M:%S")/g' {} \; 2>/dev/null
 
-# 7. 最大连接数修改为 65535
+# 6. 最大连接数修改为 65535
 if [ -f "package/base-files/files/etc/sysctl.conf" ]; then
     sed -i '/customized in this file/a net.netfilter.nf_conntrack_max=65535' package/base-files/files/etc/sysctl.conf
 fi
 
-# 8. 修改版本号展示
+# 7. 修改版本号展示
 date_version=$(date +"%y.%m.%d")
 if [ -f "package/immortalwrt/default-settings/files/zzz-default-settings" ]; then
     orig_version=$(cat "package/immortalwrt/default-settings/files/zzz-default-settings" | grep DISTRIB_REVISION= | awk -F "'" '{print $2}')
@@ -60,7 +57,7 @@ elif [ -f "package/lean/default-settings/files/zzz-default-settings" ]; then
     sed -i "s/${orig_version}/R${date_version} by herofence/g" package/lean/default-settings/files/zzz-default-settings
 fi
 
-# 9. 取消主题默认配置覆盖
+# 8. 取消主题默认配置覆盖
 find package/luci-theme-*/* -type f -name '*luci-theme-*' -print -exec sed -i '/set luci.main.mediaurlbase/d' {} \; 2>/dev/null
 
 echo "脚本执行完成"
