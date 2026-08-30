c297221235b979527c774f2d843abb3d7c477c6c
herofence <113574064+herofence@users.noreply.github.com>
2026-08-30 08:10:37 +0800

    Update diy-part2.sh

--- scripts/diy-part2.sh
+++ scripts/diy-part2.sh
@@ -10,3 +10,6 @@ find feeds/luci/ -path "*/system.lua" -exec sed -i 's/os.date("%c")/os.date("%Y-
 
 # 3. 修改默认IP
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
+
+# 修改主机名字，把herofence修改你喜欢的就行（不能纯数字或者使用中文）
+# sed -i s/OpenWrt/herovence/g package/base-files/files/bin/config_generate
